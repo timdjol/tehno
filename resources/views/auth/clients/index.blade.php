5e1088f9dc2ef6ff51c35796d03432dc1c8a839e
Tim Djol <tdjol007@gmail.com>
2024-03-30 01:23:57 +0600

    clients

--- resources/views/auth/clients/index.blade.php
+++ resources/views/auth/clients/index.blade.php
@@ -2,7 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                <div class="row">
 
+                </div>
             </div>
         </div>
     </div>
